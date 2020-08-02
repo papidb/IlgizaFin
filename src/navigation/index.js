3f7bca5b40ae0d9f1cf2bf9674ca118fc8a44d3d
Benjamin Daniel <benjamindaniel706@gmail.com>
2020-08-02 15:58:37 +0100

    added basic navigation and navigation stack

---
+++ src/navigation/index.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {OnBoarding} from '../screens/OnBoarding';
+import {Home} from '../screens/Home';
+
+const Stack = createStackNavigator();
+
+function Nav() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{stackAnimation: 'fade', headerShown: false}}>
+        <Stack.Screen name="OnBoarding" component={OnBoarding} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Nav;
